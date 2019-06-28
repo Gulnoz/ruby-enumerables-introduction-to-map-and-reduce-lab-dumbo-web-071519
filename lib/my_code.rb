09d33466a3138a1c123e09f20e64689be3cbf93b
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-06-28 01:48:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,31 +2,32 @@
 
 def map_to_negativize(arr)
   new_arr=[]
-   
   for i in arr do 
     new_arr.push(i*(-1))
   end
-
   return new_arr
 end
+
 def map_to_no_change(arr)
   return arr
 end
 
 def map_to_double(arr)
-  new_arr=[]
+   new_arr=[]
    for i in arr do 
-    new_arr.push(i*2)
-  end
-  return new_arr
+     new_arr.push(i*2)
+   end
+   return new_arr
 end
+
 def map_to_square(arr)
-  new_arr=[]
+   new_arr=[]
    for i in arr do 
-    new_arr.push(i**2)
+     new_arr.push(i**2)
   end
   return new_arr
 end
+
 def reduce_to_total(arr,start=0)
   total=start
   for i in arr do 
@@ -34,6 +35,7 @@ def reduce_to_total(arr,start=0)
   end
   return total
 end
+
 def reduce_to_all_true(arr)
   for i in arr do 
     if(!i)
@@ -42,6 +44,7 @@ def reduce_to_all_true(arr)
   end
   return true
 end
+
 def reduce_to_any_true(arr)
   for i in arr do 
     if(i)
